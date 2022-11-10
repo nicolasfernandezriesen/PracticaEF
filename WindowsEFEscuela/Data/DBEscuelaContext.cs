b570220a75f43359ee7259f1aa96e5d7948962e7
Nicolas Fernandez Riesen <riesenfnicolas@gmail.com>
2022-11-10 16:30:24 -0300

    Agregar archivos de proyecto.

---
+++ WindowsEFEscuela/Data/DBEscuelaContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WindowsEFEscuela.Models;
+using System.Data.Entity;
+
+namespace WindowsEFEscuela.Data
+{
+    public class DBEscuelaContext : DbContext
+    {
+        public DBEscuelaContext() : base("keyDBEscuela") { }
+    }
+}
