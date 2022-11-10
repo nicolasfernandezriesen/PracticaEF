caaba432ffc5a8f3d463b4ac15c434cb1ece59b3
Nicolas Fernandez Riesen <riesenfnicolas@gmail.com>
2022-11-10 17:46:21 -0300

    Tarea 1. Pub, get all, insert, update, delete

--- WindowsAppPubs/Form1.Designer.cs
+++ WindowsAppPubs/Form1.Designer.cs
@@ -28,13 +28,88 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnGetAll = new System.Windows.Forms.Button();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDeleted = new System.Windows.Forms.Button();
+            this.gridPublishers = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.gridPublishers)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnGetAll
+            // 
+            this.btnGetAll.Location = new System.Drawing.Point(45, 70);
+            this.btnGetAll.Name = "btnGetAll";
+            this.btnGetAll.Size = new System.Drawing.Size(160, 80);
+            this.btnGetAll.TabIndex = 0;
+            this.btnGetAll.Text = "Traer a todos los Pubblishers";
+            this.btnGetAll.UseVisualStyleBackColor = true;
+            this.btnGetAll.Click += new System.EventHandler(this.btnGetAll_Click);
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Location = new System.Drawing.Point(281, 70);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(107, 80);
+            this.btnInsert.TabIndex = 1;
+            this.btnInsert.Text = "Crear nuevo Publisher";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(435, 70);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(110, 80);
+            this.btnUpdate.TabIndex = 2;
+            this.btnUpdate.Text = "Actualizar Publisher";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDeleted
+            // 
+            this.btnDeleted.Location = new System.Drawing.Point(603, 70);
+            this.btnDeleted.Name = "btnDeleted";
+            this.btnDeleted.Size = new System.Drawing.Size(114, 80);
+            this.btnDeleted.TabIndex = 3;
+            this.btnDeleted.Text = "Borrar Publisher";
+            this.btnDeleted.UseVisualStyleBackColor = true;
+            this.btnDeleted.Click += new System.EventHandler(this.btnDeleted_Click);
+            // 
+            // gridPublishers
+            // 
+            this.gridPublishers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridPublishers.Location = new System.Drawing.Point(45, 209);
+            this.gridPublishers.Name = "gridPublishers";
+            this.gridPublishers.RowHeadersWidth = 62;
+            this.gridPublishers.RowTemplate.Height = 28;
+            this.gridPublishers.Size = new System.Drawing.Size(672, 205);
+            this.gridPublishers.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.gridPublishers);
+            this.Controls.Add(this.btnDeleted);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnInsert);
+            this.Controls.Add(this.btnGetAll);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.gridPublishers)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnGetAll;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDeleted;
+        private System.Windows.Forms.DataGridView gridPublishers;
     }
 }
 
--- WindowsAppPubs/Form1.cs
+++ WindowsAppPubs/Form1.cs
@@ -7,14 +7,73 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WindowsAppPubs.Models;
 
 namespace WindowsAppPubs
 {
     public partial class Form1 : Form
     {
+        PubsContext context = new PubsContext();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnGetAll_Click(object sender, EventArgs e)
+        {
+            List<Publisher> publishers = context.Publishers.ToList();
+
+            gridPublishers.DataSource = publishers;
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            Publisher publisher = new Publisher() {pub_id="10", pub_name="Rokkolosss", city="Mar del Plata", country="Argentina", state="BS"};
+
+            context.Publishers.Add(publisher);
+
+            int i = context.SaveChanges();
+
+            if (i > 0)
+            {
+                MessageBox.Show("Publisher creado");
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            string id = "10";
+
+            Publisher publisher = context.Publishers.Find(id);
+            if (publisher != null) 
+            {
+                publisher.pub_name = "ElendVin";
+
+                int i = context.SaveChanges();
+
+                if (i > 0)
+                {
+                    MessageBox.Show("Nuevo nombre del publisher " + publisher.pub_name);
+                }
+            }
+        }
+
+        private void btnDeleted_Click(object sender, EventArgs e)
+        {
+            string id = "10";
+
+            var publisher = context.Publishers.Find(id);
+
+            if (publisher != null)
+            {
+                context.Publishers.Remove(publisher);
+
+                int i = context.SaveChanges();
+                if(i > 0)
+                {
+                    MessageBox.Show("El publisher " + publisher.pub_name + " se ha borrado");
+                }
+            }
         }
     }
 }
